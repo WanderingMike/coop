4c1c8d75c7bd9ab285fbaf23707754ecaf0fed7c
testuser <testuser@test.com>
2022-07-29 15:38:11 +0200

    Last version in office

--- scraper.py
+++ scraper.py
@@ -6,51 +6,79 @@ from requests_html import HTMLSession
 import pprint
 import json
 
-class Product(price, saving, title, savingText, categories):
-    self.def __init__:
-        1
+class Product:
+    def __init__(self):
+        self.title = str()
+        self.saving = float()
+        self.price = float()
+        self.savingText = str()
+        self.udoCat = list()
+
+    def sanitise(self):
+        print("sanitising")
+        self.title = self.title
+        print(self.saving)
+        print(self.price)
+        self.saving = float(self.saving)
+        self.price = float(self.price)
+
+    def clean_title(self, string):
+        string.lower()
 
 def download_data(url):
     session = requests.get(url)
     pattern = r"<meta data-pagecontent-json='(.*?)'>"
     match=re.findall(pattern, session.text)
     json_data = json.loads(match[0])
-    anchors = content["anchors"]
+    anchors = json_data["anchors"]
     return anchors
     
 
-def product_pointers(data, pointers)
+def create_products(data, pointers):
     # We need the title, the title, the original price, the new price, the saving text, the categories and the image 
-    for main_cat in range(len(data["anchors"])):
+    for main_cat in range(len(data)):
 
         products = data[main_cat]["json"]["elements"]
 
         for product in products:
 
-            product_obj = populate_product(product) 
-            pointers[product_obj.title] = product_obj
+            try:
+                product_obj = populate_product(product) 
+                print(product_obj.title)
+                pointers[product_obj.title] = product_obj
+            except:
+                print("missing this value")
 
 
 
 
-def populate_product():
+def populate_product(product):
+    
+    try:
+        obj = Product()
 
-    obj = Product()
+        for datapoint, value in product.items():
+            if datapoint in ["title", "saving", "price", "savingText", "udoCat"]:
+                exec("""obj.%s ="%s" """ % (datapoint, value))
 
-    for k,v in product.items():
-        if k in ["title", "saving", "price", "savingText", "udoCat"]:
-            print(v)
+        print(obj.title)
+    except:
+        print("missing {}".format(value))
+
+    obj.sanitise()
+    return obj
+    
 
 
 
 
 
 
-
-URL_coop = "https://www.coop.ch/en/promotions/weekly-special-offers/c/m_1011"
-content = download_data(URL_coop)
-product_pointers = dict()
-create_products(content, product_pointers)
+if __name__=="__main__":
+    URL_coop = "https://www.coop.ch/en/promotions/weekly-special-offers/c/m_1011"
+    content = download_data(URL_coop)
+    product_pointers = dict()
+    create_products(content, product_pointers)
 
 
 
